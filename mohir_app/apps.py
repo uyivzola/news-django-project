6ff3707af1d983806fbb1e7cf696b67f22d35221
uyivzola <behzodxidirov@gmail.com>
2023-06-27 17:09:57 +0500

    first commit

---
+++ mohir_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MohirAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mohir_app'
---
+++ mohir_app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class News(models.Model):
+    class Status(models.TextChoices):
+        Draft = 'DF', 'Draft'
+        Published = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    image = models.ImageField(upload_to='mohir_app/images')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    published_time = models.DateTimeField(default=timezone.now)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices,
+                              default=Status.Draft)
+
+    class Meta:
+        ordering=['-published_time']
+
+    def __str__(self):
+        return self.title     
