1668182fa41c427764188c53c48de273987709e6
uyivzola <behzodxidirov@gmail.com>
2023-07-07 01:37:05 +0500

    add UserEditForm, ProfileAdmin

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView,  PasswordResetConfirmView, PasswordResetCompleteView
-from .views import dashboard_view, user_register
+from .views import dashboard_view, user_register,SignUpView,edit_user
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('profile', dashboard_view, name='profile'),
+    path('profile/edit', edit_user, name='edit_user_information'),
     path('signup', user_register, name='user_register'),
+#     path('signup', SignUpView.as_view(), name='user_register'),
     # PASSWORD CHANGE
     path('password-change', PasswordChangeView.as_view(), name='password_change'),
     path('password-change-done', PasswordChangeDoneView.as_view(),
