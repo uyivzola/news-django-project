3d8df78b72908cc67a222809bc4ebc97d16e75f4
uyivzola <behzodxidirov@gmail.com>
2023-07-07 01:37:19 +0500

    created Profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
-# Create your models here.
+
+# class User(AbstractUser):
+#     photo=models.ImageField()
+#     date_of_birth=models.DateTimeField()
+#     address=models.TextField()
+
+def default_profile_image():
+    # Replace 'default_image.jpg' with the actual filename of your default image
+    return 'https://m.media-amazon.com/images/M/MV5BYTdhYWU3OGEtZWZlMS00ZWM2LTliNTEtMDcyNzg0MWU0NDhkXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_FMjpg_UX1000_.jpg'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(
+        upload_to='media/users/', blank=True, null=True, default=default_profile_image)
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}' profile"
