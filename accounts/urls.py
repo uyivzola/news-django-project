d570098afbc7be6a6515b694486b94f132b161e7
uyivzola <behzodxidirov@gmail.com>
2023-07-07 21:13:38 +0500

    add classbased view of EditUserView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView,  PasswordResetConfirmView, PasswordResetCompleteView
-from .views import dashboard_view, user_register,SignUpView,edit_user
+from .views import dashboard_view, user_register, SignUpView, edit_user, EditUserView
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
     path('profile', dashboard_view, name='profile'),
-    path('profile/edit', edit_user, name='edit_user_information'),
+    #     path('profile/edit', edit_user, name='edit_user_information'),
+    path('profile/edit', EditUserView.as_view(), name='edit_user_information'),
     path('signup', user_register, name='user_register'),
-#     path('signup', SignUpView.as_view(), name='user_register'),
+    #     path('signup', SignUpView.as_view(), name='user_register'),
     # PASSWORD CHANGE
     path('password-change', PasswordChangeView.as_view(), name='password_change'),
     path('password-change-done', PasswordChangeDoneView.as_view(),
