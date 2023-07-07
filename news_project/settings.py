21e56498680bb951c9e36cd77935ea687d3bfb7c
uyivzola <behzodxidirov@gmail.com>
2023-07-07 21:14:46 +0500

    login_url for login required views

--- news_project/settings.py
+++ news_project/settings.py
@@ -143,3 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL='homePageView'
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 APPEND_SLASH = False
+
+LOGIN_URL='login'
